d3a4808127b2d33b6df76b2a2f0044179c3e8bf0
analityk <romanowskis1at@interia.pl>
2021-03-09 22:38:54 +0100

    asd

---
+++ GccLibrary1/GccLibrary1/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * GccLibrary1.cpp
+ *
+ * Created: 2021-03-04 22:15:37
+ * Author : Szymon
+ */
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
